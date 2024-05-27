f85ac6cd9382477f8e3b4e948ddc72de71db4cad
Douglas Braz <douglas.mateus29@gmail.com>
2024-05-27 05:46:21 -0300

    Created the Item app, and the Category model for it. The admin page for item was also created

---
+++ puddle/item/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories' # Changes the plural name in the Admin page
+
+    def __str__(self):
+        return self.name # For correct exhibiting each category name in the Admin page

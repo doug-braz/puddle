3943a3ac1fb1ab5dde8b77ac4fd3a3fbb0706d78
Douglas Braz <douglas.mateus29@gmail.com>
2024-06-09 10:21:42 -0300

    Items and categories visualization inserted into the index page

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from item.models import Item, Category
 
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+    return render(request, 'core/index.html', {
+        'categories' : categories,
+        'items' : items,
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')

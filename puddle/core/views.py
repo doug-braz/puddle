40e35bc0174de118e60611f0803feee36db1464c
Douglas Braz <douglas.mateus29@gmail.com>
2024-06-16 18:06:32 -0300

    Added a login view and template

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Item, Category
 from .forms import SignupForm
 
@@ -14,7 +14,15 @@ def contact(request):
     return render(request, 'core/contact.html')
 
 def signup(request):
-    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignupForm()
 
     return render(request, 'core/signup.html', {
         'form':form # For passing the form to the Front-End

ce47715a2cc5994d551694502eeb92fc55bd5e87
Douglas Braz <douglas.mateus29@gmail.com>
2024-06-19 20:10:10 -0300

    Item addition page created and fully functional

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
+from .forms import NewItemForm
+
+from django.contrib.auth.decorators import login_required
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -9,3 +12,23 @@ def detail(request, pk):
         'item' : item,
         'related_items' : related_items
         })
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False) # This is required so that the files from the form are not directly saved into the database, because not all fields from the database comes from the form - e.g. the user
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+        
+        else:
+            form = NewItemForm()
+
+    return render(request, 'item/form.html', {
+        'form':form,
+        'title': 'New item'
+    })
